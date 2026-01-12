96180cb0d4623e5214814d960b9155222918a652
amanu-nur <amanunur401@gmail.com>
2026-01-12 11:57:13 +0700

    last update

--- database/migrations/2026_01_10_000229_create_reset_tokens_table.php
+++ database/migrations/2026_01_10_000229_create_reset_tokens_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::dropIfExists('whatsapp_templates');
-        Schema::create('whatsapp_templates', function (Blueprint $table) {
+        Schema::create('reset_tokens', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('group_id')->nullable();
-            $table->text('template_type');
-            $table->text('content');
+            $table->string('email')->index();
+            $table->string('token', 255);
+            $table->timestamp('expired_at');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('whatsapp_templates');
+        Schema::dropIfExists('reset_tokens');
     }
 };
